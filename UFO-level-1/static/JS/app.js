30da7adb85fb2b1283cf2e668464ce68e8d3b662
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-09-21 11:28:26 -0400

    Update app.js

--- UFO-level-1/static/JS/app.js
+++ UFO-level-1/static/JS/app.js
@@ -1,4 +1,54 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// YOUR CODE HERE!
+var populate = (dataInput) => {
+
+	dataInput.forEach(ufo_sightings => {
+		var row = tbody.append("tr");
+		columns.forEach(column => row.append("td").text(ufo_sightings[column])
+		)
+	});
+}
+
+populate(data);
+
+button.on("click", () => {
+	d3.event.preventDefault();
+	var inputDate = inputField1.property("value").trim();
+	var inputCity = inputField2.property("value").toLowerCase().trim();
+	var filterDate = data.filter(data => data.datetime === inputDate);
+	console.log(filterDate)
+	var filterCity = data.filter(data => data.city === inputCity);
+	console.log(filterCity)
+	var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+	console.log(filterData)
+
+	tbody.html("");
+
+	let response = {
+		filterData, filterCity, filterDate
+	}
+
+	if (response.filterData.length !== 0) {
+		populate(filterData);
+	}
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+			populate(filterCity) || populate(filterDate);
+	
+		}
+		else {
+			tbody.append("tr").append("td").text("No results found!"); 
+		}
+})
+
+resetbtn.on("click", () => {
+	tbody.html("");
+	populate(data)
+	console.log("Table reset")
+})
